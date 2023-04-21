5e6b38811a9dcf605a7dd422421921e462e72846
robinn007 <124763217+robinn007@users.noreply.github.com>
2023-04-21 06:55:05 +0000

    multiply is added

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const sum = require("./lib/sum.js")
 const subtract = require("./lib/subtract.js")
+const multiply = require("./lib/multiply.js")
 
 
 console.log(sum(4, 2));
 console.log(subtract(7,2));
+console.log(multiply(7,2));
 
-module.exports = { sum, subtract}; 
+
+module.exports = { sum, subtract, multiply}; 
